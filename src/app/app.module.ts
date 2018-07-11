f243703b01d80f1a14d64dfa5f2fec4b73dc7221
Eleven Fifty <elevenfifty@Elevens-MacBook-Air.local>
2018-07-11 09:16:09 -0400

    about, nav, routing, footer - rce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    ProductsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
